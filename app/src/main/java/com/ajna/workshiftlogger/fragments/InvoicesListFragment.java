430d7f49c195c02c59fd876aea9417a33948990b
zajan <aniaaa94@gmail.com>
2019-03-12 17:12:21 +0100

    add InvoicesListFragment displaying list of files in specified folder

---
+++ app/src/main/java/com/ajna/workshiftlogger/fragments/InvoicesListFragment.java
@@ -0,0 +1,196 @@
+package com.ajna.workshiftlogger.fragments;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.ajna.workshiftlogger.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link InvoicesListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link InvoicesListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InvoicesListFragment extends Fragment {
+    private static final String TAG = "InvoicesListFragment";
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 111;
+
+    //    public static final String FOLDER_NAME = "WorkshiftLogger-INVOICES";
+    public static final String FOLDER_NAME = "PDFTest";
+
+    File[] files;
+    List<String> fileNames = new ArrayList<>();
+    ListView lvFiles;
+    ArrayAdapter<String> arrayAdapter;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public InvoicesListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment InvoicesListFragment.
+     */
+    public static InvoicesListFragment newInstance() {
+        return new InvoicesListFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int hasWriteStoragePermission = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (hasWriteStoragePermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermission();
+        } else {
+            listFiles();
+        }
+
+        arrayAdapter = new ArrayAdapter<>(
+                getContext(),
+                android.R.layout.simple_list_item_1,
+                fileNames);
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invoices_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        lvFiles = view.findViewById(R.id.lv_invoices);
+        lvFiles.setAdapter(arrayAdapter);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onNewInvoiceClicked();
+            }
+        });
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private void listFiles() {
+        String path = Environment.getExternalStorageDirectory().toString() + "/" + FOLDER_NAME;
+        Log.d(TAG, "listFiles: path = " + path);
+        File directory = new File(path);
+        files = directory.listFiles();
+        fileNames.clear();
+        if (files != null) {
+            for (File file : files) {
+                Log.d(TAG, "listFiles: name: " + file.getName());
+                fileNames.add(file.getName());
+            }
+        } else {
+            Log.d(TAG, "listFiles: files == null");
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)) {
+
+                new AlertDialog.Builder(getContext())
+                        .setMessage("To create invoices you need to allow storage access")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        REQUEST_CODE_ASK_PERMISSIONS);
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create()
+                        .show();
+            }
+
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    listFiles();
+                    arrayAdapter.notifyDataSetChanged();
+
+                } else {
+                    Toast.makeText(getContext(), "WRITE_EXTERNAL Permission Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onNewInvoiceClicked();
+    }
+}
